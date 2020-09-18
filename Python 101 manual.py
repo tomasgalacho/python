48c2d5c606d1b55d2131612f2e24a1c0407d2af1
Tomás Galacho <71157648+tomasgalacho@users.noreply.github.com>
2020-09-18 19:42:02 -0300

    Add files via upload

---
+++ Python 101 manual.py
@@ -0,0 +1,114 @@
+    ### Python 101 manual
+
+## Variables and data types
+
+#character_name = "John"#
+#character_age = "50"#
+#is_male = False#
+#print("There once was a man named " + character_name + ", ")#
+#print("he was " + character_age + " years old ")#
+
+
+## Working with strings
+
+#print("The umbrella\nAcademy")
+
+#phrase = "The umbrella Academy"
+#print(phrase + " is cool")
+#print(phrase.upper())
+
+
+## Working with numbers
+
+#print(-2.524)
+#print(3*(4+5))
+#print(10%3)
+
+#my_num = 5
+#print(str(my_num) + " is my favourite number")
+#print(pow(my_num, 2))
+#print(max(4, 6))
+#print(round(3.66))
+
+#from math import *
+#print(sqrt(36))
+
+
+## Getting Input from users
+
+#name = input("Enter your name: ")
+#age = input("Enter your age: ")
+
+#print("Hello " + name + "! You are " + age)
+
+
+## Building a basic calculator
+
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result = float(num1) + float(num2)
+
+#print(result)
+
+
+## Mad Libs Game
+
+#color = input("Enter a color ")
+#plural_noun = input("Enter a plural noun ")
+#celebrity = input("Enter a celebrity ")
+
+#print("Roses are " + color)
+#print(plural_noun + " are blue")
+#print("and I love " + celebrity)
+
+
+## Lists
+
+#friends = ["Kevin", "Karen", "Jim"]
+                        
+#print(friends[0:1])
+#print(friends[1:])
+
+
+## Lists Functions
+
+#lucky_numbers = [4, 8, 15, 16, 23, 42]
+#friends = ["Kevin", "Kevin", "Karen", "Jim", "Oscar", "Toby"]
+#friends.extend(lucky_numbers)
+#friends.append("Creed")
+#friends.insert(1, "Kelly")
+#friends.remove("Jim")
+#friends.pop()
+#friends.index("Kevin")
+#friends.sort()
+#lucky_numbers.sort()
+#lucky_numbers.reverse()
+
+#friends2 = friends.copy()
+
+#print(friends)
+#print(lucky_numbers)
+#print(friends.count("Kevin"))
+
+#print(friends2)
+
+
+## Tuples
+
+#coordinates = [(4, 5), (6, 7), (80, 34)]
+#print(coordinates[0])
+
+
+## Functions = def
+
+#def say_hi(name, age):
+    #print("Hello " + name + ", you are " + age)
+
+#say_hi("Tom", "20")
+#say_hi("Steve", "35")
+
+
+## Return statement
+
+
+
